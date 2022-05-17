1954d512d2c888227d267badfdcd85aeace30142
JQUAN CALEK <jquancollins96@gmail.com>
2022-05-16 23:24:31 -0400

    Stuck on the bottle

--- server.js
+++ server.js
@@ -47,9 +47,27 @@ const myArray = [
 
 app.get("/magic/:question", (request, response) => {
   const x = Math.floor(Math.random() * myArray.length - 1);
-  response.send(`${request.params.question}  <h1> ${myArray[x]}</h1>`);
+  response.send(`${request.params.question}  <h1> ${myArray[x]} </h1>`);
 });
 
 app.listen(3000, () => {
   console.log("Hello, Seattle. I'm listening for what");
 });
+
+// On the home page (get "/"), users should see:
+//"99 Bottles of beer on the wall"
+app.get("/number_of_bottles/:98", (request,response)=>{}
+)
+    // a link that says "take one down, pass it around"
+ 
+
+  //this should link to /98, where the number represents the number of bottles left.
+
+  //When a number is given in the url (get "/:number_of_bottles"), users should see:
+//The number of bottles of beer on the wall (i.e. 98 Bottles of beer on the wall.)
+
+
+//a link to "take one down, pass it around",
+// where the href is number of bottles in the parameter minus 1.
+
+
